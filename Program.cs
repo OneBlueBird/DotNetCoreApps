e33512ebf96b4370255ccb9c1f8d93734bad7966
OneBlueBird <saravanakumar.murugesan@gmail.com>
2020-04-17 21:33:18 +0530

    Initial Version..

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CloudifyConsoleApps
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Cloudify");
+            Console.WriteLine("Done..");
+        }
+    }
+}
